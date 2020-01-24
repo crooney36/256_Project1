a2cd6d04c8f0d2f8b33248a6df823b9269efcf29
crooney36 <56104142+crooney36@users.noreply.github.com>
2020-01-24 10:11:26 -0500

    Add files via upload
    100% on Project1

--- Employee.java
+++ Employee.java
@@ -17,16 +17,18 @@ public class Employee extends Person {
     // Default constructor for Employee object.
     public Employee(){
         super();
-        office = "Unassigned";
-        salary = 0;
-        hireDate = null;
-
+        this.office = "Unassigned";
+        this.salary = 0;
+        this.hireDate = null;
     }
     // Parameterized constructor for Employee object.
     public Employee(String first, String middle, String last , Address address, String phoneNumber, String email, String office, int salary, int month, int day, int year){
         super(first, middle, last, address, phoneNumber, email);
         this.office = office;
-        this.salary = salary;
+        if(salary < 0) {
+            throw new IllegalArgumentException("Salary must be a positive integer.");
+        }
+            this.salary = salary;
         hireDate = LocalDate.of(year, month, day);
     }
     // Inherits the toString method from Person class and adds the additional variables from the Employee class.
